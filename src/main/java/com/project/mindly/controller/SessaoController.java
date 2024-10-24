a473e5e08acbb8d7b06567b056562c7b1f241537
Alan Cruz <alancruz495@gmail.com>
2024-10-24 11:41:23 -0300

    feat: criacao do recurso sessao

--- src/main/java/com/project/mindly/controller/SessaoController.java
+++ src/main/java/com/project/mindly/controller/SessaoController.java
@@ -2,7 +2,11 @@ package com.project.mindly.controller;
 
 import com.project.mindly.model.sessao.Sessao;
 import com.project.mindly.repository.SessaoRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +28,11 @@ public class SessaoController {
         return sessaoRepository.findAll();
     }
 
-
+    @GetMapping("/id")
+    public ResponseEntity<Sessao> getByIdSessao(@PathVariable @Valid int id) {
+        return sessaoRepository.findById(id)
+                .map(result -> ResponseEntity.status(HttpStatus.OK).body(result))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 
 }
